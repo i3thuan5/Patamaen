40161fc795b89f0638be6b8be2712329505f7209
Fafofafoy <ihcaoe@gmail.com>
2022-03-08 11:33:02 +0800

    用template。

--- patamaen/falic/views.py
+++ patamaen/falic/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
 def falic(request):
-    return HttpResponse('Fafoy')
+    return render(request, template_name='patamaen/falic.html')
