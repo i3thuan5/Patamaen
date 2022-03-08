00f52b2e810a5f1738df3273ca93f1eb4badae6d
Fafofafoy <ihcaoe@gmail.com>
2022-03-08 11:25:25 +0800

    加view

--- patamaen/falic/views.py
+++ patamaen/falic/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def falic(request):
+    return None
--- patamaen/patamaen/urls.py
+++ patamaen/patamaen/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from falic.views import falic
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('falic', falic, name='falic')
 ]
