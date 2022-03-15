ca1be46fe6dc1e91ff72c508187ed33d235a0e77
Fafofafoy <ihcaoe@gmail.com>
2022-03-15 15:46:04 +0800

    flake8

--- patamaen/patamaen/settings-pasadaken.py
+++ patamaen/patamaen/settings-pasadaken.py
@@ -1,3 +1,3 @@
-from .settings import *
+from .settings import * # noqa
 
 DEBUG = False
--- patamaen/patamaen/settings.py
+++ patamaen/patamaen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kuzbz_+5tmvw8w@n)4qw-o_j80c=l8a0*8t*z92hq9gwmc*p6n'
+SECRET_KEY = 'django-insecure-kuzbz_+5tm8w@n)4qw-o_j80c=l8a0*8t*z92hq9gwmc*p6n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,16 +91,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
     },
 ]
 
