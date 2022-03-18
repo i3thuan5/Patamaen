a6c14352d77eb41331ed788ff0c20da8787abe26
Fafofafoy <ihcaoe@gmail.com>
2022-03-18 14:49:37 +0800

    Template 資料抽去 view。

--- patamaen/falic/views.py
+++ patamaen/falic/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def falic(request):
-    return render(request, template_name='patamaen/falic.html')
+    return render(
+        request,
+        template_name='patamaen/falic.html',
+        context={'sentence': "Tomay nga'ay ho,komaen kiso haw?"},
+    )
