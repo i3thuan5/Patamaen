1902eb70c45cd589ea27811bff3446570ec7b0ab
Fafofafoy <ihcaoe@gmail.com>
2022-03-18 15:58:01 +0800

    Mitongal to diff.

--- patamaen/falic/views.py
+++ patamaen/falic/views.py
@@ -1,27 +1,55 @@
 from django.shortcuts import render
 import html
+import diff_match_patch as dmp_module
 
 
 def falic(request):
     sasalofen = "nga'ay ho ,komaen kiso haw?"
     nisalofan = "Nga'ay ho, komaen kiso haw?"
-    import diff_match_patch as dmp_module
 
     dmp = dmp_module.diff_match_patch()
-    diff = dmp.diff_main("Hello World.", "Goodbye World.")
+    diff = dmp.diff_main(sasalofen, nisalofan)
+
     print(diff)
+    diff = [
+        (-1, 'n'),
+        (1, 'N'),
+        (0, "ga'ay ho"), 
+        (-1, ' '), 
+        (0, ','), 
+        (1, ' '), 
+        (0, 'komaen kiso haw?')
+    ]
+    nisalofan = []
+    for codad in diff:
+        if codad[0] == -1:
+            # nisalofan.append(
+            #     ('span', 'cakatama', codad[1])
+            # )
+            pass
+        elif codad[0] == 0:
+            nisalofan.append(
+                ('span', '', codad[1])
+            )
+        elif codad[0] == 1:
+            nisalofan.append(
+                ('span', 'cakatama', codad[1])
+            )
+
+
     return render(
         request,
         template_name='patamaen/falic.html',
         context={
             'sasalofen': sasalofen,
-            'nisalofan': [
-                ('span', 'cakatama', 'N'),
-                ('span', '', "ga'ay ho"),
-                ('del', 'caaykatama', html.escape(' ')),
-                ('span', '', ','),
-                ('span', 'cakatama', html.escape(' ')),
-                ('span', '', 'komaen kiso haw?'),
-            ]
+            'nisalofan': nisalofan,
         },
     )
+    # [
+    #             ('span', 'cakatama', 'N'),
+    #             ('span', '', "ga'ay ho"),
+    #             ('del', 'caaykatama', html.escape(' ')),
+    #             ('span', '', ','),
+    #             ('span', 'cakatama', html.escape(' ')),
+    #             ('span', '', 'komaen kiso haw?'),
+    #         ]
