5fe2e00030b94971d81c34d43533bc3718d5f4aa
Fafofafoy <ihcaoe@gmail.com>
2022-03-22 11:23:06 +0800

    Alaen to label.

--- patamaen/falic/forms.py
+++ patamaen/falic/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class MatamaForm(forms.Form):
     sasalofen = forms.CharField(
-        label='Sasalofen', max_length=100,
+        label='Codaden itini ko sasalofen no miso a codad', max_length=100,
         widget=forms.Textarea(attrs={
             'class': 'form-control',
             'placeholder': "nga'ay ho ,o maan ko sakalafi no miso?",
