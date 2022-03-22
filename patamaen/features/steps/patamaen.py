95b76d221e7c82b8d34d90e1d0955704c255ca45
Fafofafoy <ihcaoe@gmail.com>
2022-03-22 16:55:23 +0800

    Patongal to step.

---
+++ patamaen/features/steps/patamaen.py
@@ -0,0 +1,11 @@
+from behave import when, then
+
+
+@when('Mitilid ko tamdaw {tilid}')
+def Mitilid(context, tilid):
+    raise NotImplementedError(u'STEP: When Mitilid ko tamdaw Ci Panay kako ,nga\'ay ho?')
+
+
+@then("改 to {matamaay}")
+def 改(context, matamaay):
+    raise NotImplementedError(u'STEP: Then 改 to Ci Panay kako, nga\'ay ho?')
