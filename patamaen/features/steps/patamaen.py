5e2023e38635267e72d9731ab3dca38b130ad474
Fafofafoy <ihcaoe@gmail.com>
2022-03-23 08:57:22 +0800

    Patongal to browser no selenium.

--- patamaen/features/steps/patamaen.py
+++ patamaen/features/steps/patamaen.py
@@ -1,11 +1,19 @@
 from behave import when, then
 
+from time import sleep
+
 
 @when('Mitilid ko tamdaw {tilid}')
 def Mitilid(context, tilid):
-    raise NotImplementedError(u'STEP: When Mitilid ko tamdaw Ci Panay kako ,nga\'ay ho?')
+    print(context.tsuki + '/falic')
+    context.browser.get(context.tsuki + '/falic')
+    sleep(100)
+    context.browser.find_element_by_id('id_sasalofen').send_keys('tilid')
+    context.browser.find_element_by_name('button').click()
 
 
 @then("改 to {matamaay}")
 def 改(context, matamaay):
+    print(context.browser.find_element_by_name('button').text())
     raise NotImplementedError(u'STEP: Then 改 to Ci Panay kako, nga\'ay ho?')
+    context.browser.find_element_by_name('button').text()
